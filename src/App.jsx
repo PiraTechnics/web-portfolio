c0e3dced7a17250d067d62d41c322dfb2e6a96ac
PiraTechnics <devin1younge@gmail.com>
2023-08-17 15:17:13 -0700

    Bootstrap Dark Menu Bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,53 @@
-import { Container, Row, Navbar } from "react-bootstrap";
+import { Container, Row, Image } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
-import './App.css';
+import "./App.css";
 import AppLink from "./AppLink";
 import WetterIcon from "./assets/wetter-icon.png";
-import PiraTechnics from "./assets/piratechnics.png";
-import { Image } from 'react-bootstrap';
 import { FcTodoList, FcCalculator } from "react-icons/fc";
 import { TbCrystalBall } from "react-icons/tb";
 import { GiBookshelf } from "react-icons/gi";
-
+import Header from "./Header";
 
 function App() {
-
   return (
     <>
-      <Navbar bg="info-subtle" variant="light" fixed='top' className='py-1'>
-        <Container fluid>
-          <Navbar.Brand href="#home" className='py-0 align-text-bottom'>
-            <Image roundedCircle fluid src={PiraTechnics} width={75} alt="Devin Younge"></Image>
-            <span className='px-2 fs-2 text-decoration-underline'>Devin Younge</span>
-          </Navbar.Brand>
-        </Container>
-      </Navbar>
+      <Header />
       <Container fluid style={{ marginTop: "75px" }}>
         <Row>
-          <AppLink icon={<Image src={WetterIcon} alt="wetter cloud icon"></Image>} link={"https://piratechnics.github.io/wetter"} title={"Wetter"} subtitle={"The Better Weather App"} />
-          <AppLink icon={<FcTodoList size={75} />} link={"https://piratechnics.github.io/Prio/"} title={"Prio"} subtitle={"Your Tasklist, Prioritized"} />
-          <AppLink icon={<TbCrystalBall size={65} />} link={"https://piratechnics.github.io/wisdomball/"} title={"Wisdomball"} subtitle={"A most ponderous Orb"}></AppLink>
-          <AppLink icon={<GiBookshelf size={65} color={"#624a2e"} />} link={"https://piratechnics.github.io/BibLog/"} title={"BibLog"} subtitle={"Record your reading"}></AppLink>
-          <AppLink icon={<FcCalculator size={75} />} link={"https://piratechnics.github.io/odin-project-calculator/"} title={"Calc5000"} subtitle={"Crunch the Numbers"}></AppLink>
+          <AppLink
+            icon={<Image src={WetterIcon} alt="wetter cloud icon"></Image>}
+            link={"https://piratechnics.github.io/wetter"}
+            title={"Wetter"}
+            subtitle={"The Better Weather App"}
+          />
+          <AppLink
+            icon={<FcTodoList size={75} />}
+            link={"https://piratechnics.github.io/Prio/"}
+            title={"Prio"}
+            subtitle={"Your Tasklist, Prioritized"}
+          />
+          <AppLink
+            icon={<TbCrystalBall size={65} />}
+            link={"https://piratechnics.github.io/wisdomball/"}
+            title={"Wisdomball"}
+            subtitle={"A most ponderous Orb"}
+          ></AppLink>
+          <AppLink
+            icon={<GiBookshelf size={65} color={"#624a2e"} />}
+            link={"https://piratechnics.github.io/BibLog/"}
+            title={"BibLog"}
+            subtitle={"Record your reading"}
+          ></AppLink>
+          <AppLink
+            icon={<FcCalculator size={75} />}
+            link={"https://piratechnics.github.io/odin-project-calculator/"}
+            title={"Calc5000"}
+            subtitle={"Crunch the Numbers"}
+          ></AppLink>
         </Row>
-      </Container >
+      </Container>
     </>
-  )
+  );
 }
 
 export default App;
---
+++ src/Header.jsx
@@ -0,0 +1,43 @@
+import PiraTechnics from "./assets/pira-icon.png";
+import { Container, Navbar, Nav, Image } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+
+const Header = () => {
+  return (
+    <Navbar
+      expand="sm"
+      bg="dark"
+      data-bs-theme="dark"
+      fixed="top"
+      className="py-1"
+    >
+      <Container fluid className="px-0">
+        <Navbar.Brand href="#home" className="ps-2">
+          <Image
+            fluid
+            src={PiraTechnics}
+            width={50}
+            alt="Devin Younge Logo"
+          ></Image>{" "}
+          <span className="px-2 fs-4">Devin Younge</span>
+        </Navbar.Brand>
+        <Navbar.Toggle
+          aria-controls="top-nav-toggler"
+          className="me-3 border border-0"
+        />
+        <Navbar.Collapse
+          id="to-nav-toggler"
+          className="justify-content-end my-1 border-top"
+        >
+          <Nav>
+            <Nav.Link href="#about">About</Nav.Link>
+            <Nav.Link href="#blah">Blah</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default Header;
