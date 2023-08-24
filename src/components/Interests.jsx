9ec70d24fd0f49f5a1572aff44f10ec98efd8a9c
PiraTechnics <devin1younge@gmail.com>
2023-08-24 10:38:37 -0700

    Interests Section Cleanup

--- src/components/Interests.jsx
+++ src/components/Interests.jsx
@@ -12,8 +12,19 @@ const Interests = () => {
       className="py-5 mt-3 section-container d-flex flex-column"
       fluid
     >
-      <Row className="p-5 pt-3">
-        <Col xs={8} sm={6} lg={3} className="my-5 mx-auto">
+      <Row>
+        <div className="text-center">
+          <h2>Interests</h2>
+          My hobbies and passions outside of coding
+        </div>
+      </Row>
+      <Row className="p-5 pt-3 justify-content-center">
+        <Col
+          xs={10}
+          sm={6}
+          lg={3}
+          className="my-5 d-flex align-items-center justify-content-center"
+        >
           <Card className="interest-card">
             <Card.Img
               variant="top"
@@ -30,7 +41,12 @@ const Interests = () => {
             </Card.Body>
           </Card>
         </Col>
-        <Col xs={10} sm={6} lg={3} className="my-5 mx-auto">
+        <Col
+          xs={10}
+          sm={6}
+          lg={3}
+          className="my-5 d-flex align-items-center justify-content-center"
+        >
           <Card className="interest-card">
             <Card.Img
               variant="top"
@@ -39,7 +55,7 @@ const Interests = () => {
             />
             <hr className="m-1 border-2 opacity-50" />
             <Card.Body className="card-body">
-              <Card.Title>Literature</Card.Title>
+              <Card.Title>Reading</Card.Title>
               <Card.Text>
                 I collect and read quality editions of fantasy, horror and
                 science-fiction stories.
@@ -47,7 +63,12 @@ const Interests = () => {
             </Card.Body>
           </Card>
         </Col>
-        <Col xs={10} sm={6} lg={3} className="my-5 mx-auto">
+        <Col
+          xs={10}
+          sm={6}
+          lg={3}
+          className="my-5 d-flex align-items-center justify-content-center"
+        >
           <Card className="interest-card">
             <Card.Img
               variant="top"
@@ -58,13 +79,18 @@ const Interests = () => {
             <Card.Body className="card-body">
               <Card.Title>Video Games</Card.Title>
               <Card.Text>
-                I enjoy PC and console gaming, and made a career of live
-                commentating Esports events.
+                I enjoy PC and console games, any anything with a big world to
+                explore.
               </Card.Text>
             </Card.Body>
           </Card>
         </Col>
-        <Col xs={10} sm={6} lg={3} className="my-5 mx-auto">
+        <Col
+          xs={10}
+          sm={6}
+          lg={3}
+          className="my-5 d-flex align-items-center justify-content-center"
+        >
           <Card className="interest-card">
             <Card.Img
               variant="top"
@@ -76,7 +102,7 @@ const Interests = () => {
               <Card.Title>Travel</Card.Title>
               <Card.Text>
                 I've been to <strong>19</strong> countries,
-                <strong>13</strong> US states, 42 cities worldwide, and
+                <strong>13</strong> US states, <strong>42</strong> cities and
                 counting!
               </Card.Text>
             </Card.Body>
